2a830c698c42813d772a1bd63fd393014e826dab
EvgenKor <evgen2k7@yandex.ru>
2016-05-17 22:59:31 +0300

    bugfix (task with NSDate object)

--- src/ios/InterviewCordova.m
+++ src/ios/InterviewCordova.m
@@ -13,11 +13,7 @@
 - (void)showDemoInterview:(CDVInvokedUrlCommand*)command{
     __block CDVPluginResult* pluginResult = nil;
     [[Interview instance] showInterviewControllerWithTaskId:nil animated:YES withSuccessHandler:^(NSDictionary * _Nullable task) {
-        if(task){
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:task]; // interview passed
-        }else{
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];  // canceled by user
-        };
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     } andErrorHandler:^(id  _Nullable error) {
         if([error isKindOfClass:[NSString class]]){
